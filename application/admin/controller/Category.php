d865872e6367d36063fde62fdae8a6f93ee3cd93
yjjmuz <401418253@qq.com>
2018-11-17 16:48:40 +0800

    11-17

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -48,4 +48,36 @@ class Category extends Controller
         $this->assign('data',$data);
         return $this->fetch();
     }
+    public function edit()
+    {
+        if(request() ->isPost())
+        {
+//             halt($_POST);
+            $res = $this->db->edit(input('post.'));
+            if($res['valid']){
+                $this->success($res['msg'],'index');exit;
+            }else{
+                $this->error($res['msg']);exit;
+            }
+        }
+        $cate_id = input('param.cate_id');
+        $oldData = $this->db->find($cate_id);
+        dump($oldData);
+        $this->assign('oldData',$oldData);
+        $cateData = $this->db->getCateData($cate_id);
+//         halt($cateData);
+        $this->assign('cateData',$cateData);
+        return $this->fetch();
+    }
+    public function del()
+    {
+//         halt(input('param.cate_id'));
+        $res = $this->db->del( input('param.cate_id'));
+        if($res['valid']){
+//             dump($res);
+            return $this->success($res['msg'],'index');
+        }else{
+            return $this->error($res['msg'],'index');        
+        }
+    }
 }
--- application/common/model/Category.php
+++ application/common/model/Category.php
@@ -2,6 +2,7 @@
 namespace app\common\model;
 
 use think\Model;
+use function think\startTrans;
 
 class Category extends Model
 {
@@ -18,6 +19,52 @@ class Category extends Model
             return ['valid'=>0,'msg'=>$this->getError()];
         }else{
             return ['valid' =>1, 'msg' => '添加成功'];
+        }
+    }
+    public function getCateData($cate_id)
+    {
+//         halt(db('cate')->select());
+        $cate_ids = $this->getSon(db('cate')->select(),$cate_id);
+        $cate_ids[] = $cate_id;
+        $field = db('cate')->whereNotIn('cate_id', $cate_ids)->select();
+//         halt($field);
+        return $field;
+    }
+    public function getSon($data,$cate_id)
+    {
+        static $temp = [];
+        foreach ($data as $k => $v)
+        {
+            if($cate_id == $v['cate_pid'])
+            {
+                $temp[] = $v['cate_id'];
+                $this->getSon($data, $v['cate_id']);
+            }
+        }
+        return $temp;
+    }
+    
+    public function edit($data)
+    {
+        $result = $this->validate(true)->save($data,[$this->pk=>$data['cate_id']]);
+        if($result)        {
+            return ['valid'=>1,'msg'=>'编辑成功'];
+        }else{
+            return ['valid'=>0,'msg'=>$this->getError()];
+        }
+    }
+    
+    public function del($cate_id)
+    {
+//         dump($cate_id);
+        $cate_pid = $this->where('cate_id',$cate_id)->value('cate_id');
+        
+        $this->where('cate_pid',$cate_id) ->update(['cate_pid' => $cate_pid]);
+        
+        if(Category::destroy($cate_id)){
+            return ['valid' =>1,'msg'=>'删除成功'];
+        }else{
+            return ['valid' =>0,'msg'=>'删除失败'];
         }
     }
 }
